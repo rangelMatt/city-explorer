79205b7ef52b4b3a9b349b31697357ee51140c50
Matt Rangel <95889943+rangelMatt@users.noreply.github.com>
2022-02-21 23:06:04 -0800

    working it

--- src/Main.js
+++ src/Main.js
@@ -9,14 +9,15 @@ class Main extends React.Component {
     super(props);
     this.state = {
       locationData: [],
+      query: ''
     };
   }
 
   requestData = async (searchTerms) => {
     try {
-    let locationIqData = await axios.get('https://us1.locationiq.com/v1/search.php?key=pk.2823f2cb80c2a3d979df18eb23cff40c&q=Arroyo%20Grande&format=json')
+    let locationIqData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.query}&format=json`)
     console.log(searchTerms)
-    console.log(locationIqData.data)
+    console.log(locationIqData.data)   
     this.setState({
       locationData: locationIqData.data[0],
     })
@@ -24,13 +25,17 @@ class Main extends React.Component {
     console.log('Wrongo, thats incorrect')
   }
 }
+  handleQuery = (e) => {
+    this.setState({query: e})
+  }
+
   handleSubmit = (e) => {
     e.preventDefault()
-    this.requestData(e.target[0].value);
+    this.requestData(e.target.value);
   }
 
   render() {
-  
+    console.log(this.state)
     return (
       <main className="m-3">
         <Form className="m-3 w-25" onSubmit ={this.handleSubmit}>
@@ -42,6 +47,7 @@ class Main extends React.Component {
             <Form.Control
               id="cityInput"
               placeholder="Someplace"
+              onInput={(e)=>{this.handleQuery(e.target.value)}} 
             />
 
           </Form.Group>
