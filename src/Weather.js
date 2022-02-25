1ee2a49d0ddbb12d7314c5fa19684b444f52c2a6
Matt Rangel <95889943+rangelMatt@users.noreply.github.com>
2022-02-25 13:36:03 -0800

    refactor

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,16 @@
-// import React from "react";
+import React from "react";
 
-// import ListGroup from "react-bootstrap/ListGroup";
+import ListGroup from "react-bootstrap/ListGroup";
 
-// class Weather extends React.Component {
-//   render() {
-//     let forecast = this.props.forecast;
-//     return (
-//       <>
-//         <ListGroup.Item> {forecast.date}: Low of {forecast.low}, and a High of {forecast.high}, with {forecast.description}</ListGroup.Item>
-//       </>
-//     );
-//   }
-// }
+class Weather extends React.Component {
+  render() {
+    let forecast = this.props.forecast;
+    return (
+      <>
+        <ListGroup.Item> {forecast.date}: Low of {forecast.low}, and a High of {forecast.high}, with {forecast.description}</ListGroup.Item>
+      </>
+    );
+  }
+}
 
-// export default Weather;
+export default Weather;
