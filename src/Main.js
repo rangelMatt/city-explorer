4f6fc97bfaac88f1ff013304e7f6eff25e661417
Matt Rangel <95889943+rangelMatt@users.noreply.github.com>
2022-02-24 20:01:36 -0800

    diverged

--- src/Main.js
+++ src/Main.js
@@ -49,26 +49,14 @@ class Main extends React.Component {
         weatherData: results.data,
         renderWeather: true,
       })
-    } catch(error) {
-      this.setState ({
+    } catch (error) {
+      this.setState({
         weatherError: true,
         weatherErrorMessage: `A Weather Error Occured: ${error.response.status}, ${error.response.data}`
       })
-      
-    }
-    
-  }
 
-  requestData = async (searchTerms) => {
-    try {
-      let locationIqData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.query}&format=json`)
-      // let weatherData = await axios.get(``)
-      this.setState({
-        locationData: locationIqData.data[0],
-      })
-    } catch (error) {
-      console.log('Wrongo, thats incorrect')
     }
+
   }
 
 
@@ -94,45 +82,45 @@ class Main extends React.Component {
 
     return (
       <>
-      <main className="m-3">
-        <Form className="m-3 w-25" onSubmit={this.handleSubmit}>
-          <Form.Group>
-            <Form.Label htmlFor="cityInput">
-              Enter City Name
-            </Form.Label>
-            <button type="submit"
-              className="mt-3">Find Me!
-            </button>
-            <Form.Control
-              id="cityInput"
-              placeholder="Someplace"
-              onInput={(e) => { this.handleQuery(e.target.value) }}
-            />
+        <main className="m-3">
+          <Form className="m-3 w-25" onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Form.Label htmlFor="cityInput">
+                Enter City Name
+              </Form.Label>
+              <button type="submit"
+                className="mt-3">Find Me!
+              </button>
+              <Form.Control
+                id="cityInput"
+                placeholder="Someplace"
+                onInput={(e) => { this.handleQuery(e.target.value) }}
+              />
 
-          </Form.Group>
-        </Form>
+            </Form.Group>
+          </Form>
 
-        <Map locationData={this.state.locationData} />
+          <Map locationData={this.state.locationData} />
 
-        {
-          this.statedisplayCityData &&
-          <Card>
-          <Card.Body>
-            <Card.Title>{this.state.cityData.display_Name}</Card.Title>
-            <Card.Text>
-            {this.url}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        }
-        {
-          this.state.renderWeather &&
-          <ListGroup className="m-md-auto w50">
-            {dailyForecasts}
-          </ListGroup>
-        }
-        <h3>{this.state.weatherErrorMessage}</h3>
-      </main>
+          {
+            this.statedisplayCityData &&
+            <Card>
+              <Card.Body>
+                <Card.Title>{this.state.cityData.display_Name}</Card.Title>
+                <Card.Text>
+                  {this.url}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          }
+          {
+            this.state.renderWeather &&
+            <ListGroup className="m-md-auto w50">
+              {dailyForecasts}
+            </ListGroup>
+          }
+          <h3>{this.state.weatherErrorMessage}</h3>
+        </main>
       </>
     );
   }
